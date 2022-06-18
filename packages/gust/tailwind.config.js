35ccafce59fb8a40b798ea36baf00cc6c9259d5d
Walee Ahmed <walee@labxd.com>
2022-06-18 17:45:03 -0400

    feat(gustxd): pkg setup 🍃

---
+++ packages/gust/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ["./src/**/*.{html,js,tsx,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = config;
---
+++ packages/gust/tsdx.config.js
@@ -0,0 +1,23 @@
+const postcss = require("rollup-plugin-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const tailwindcss = require("tailwindcss");
+
+const tailwindConfig = require("./tailwind.config.js");
+const path = require("path");
+
+module.exports = {
+  rollup(config, options) {
+    config.plugins.push(
+      postcss({
+        modules: true,
+        plugins: [tailwindcss(tailwindConfig), autoprefixer()],
+        inject: true,
+        // only write out CSS for the first bundle (avoids pointless extra files):
+        extract: !!options.writeMeta,
+      })
+    );
+
+    return config;
+  },
+};
