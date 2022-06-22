8298ca989cde503fd4fa23ca952e4ad71d35898f
Jimmy N <jamesfrontend@gmail.com>
2022-06-22 13:16:50 -0400

    chore: copy and safelist

--- packages/gustxd/presets.js
+++ packages/gustxd/presets.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
   plugins: [require("@tailwindcss/forms")],
+  safelist: [
+    {
+      pattern: /bg-*-*/,
+    },
+  ],
 };
 
 module.exports = config;
